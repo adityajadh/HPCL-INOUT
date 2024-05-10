9e1a7880cc3bb87981fb73e7998813a11e7bf6a7
adityajadh <126766073+adityajadh@users.noreply.github.com>
2024-05-10 12:32:46 +0530

    testupdate

--- src/main/java/com/in_out/HpclInOutApplication.java
+++ src/main/java/com/in_out/HpclInOutApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HpclInOutApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HpclInOutApplication.class, args);;
+		SpringApplication.run(HpclInOutApplication.class, args);
 	}
 
 }
